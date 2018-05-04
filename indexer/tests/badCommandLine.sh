7031564655083952f1c1c3e5ed4617d29650d399
Shengsong Gao <Shengsong.Gao.21@dartmouth.edu>
2018-05-04 17:52:11 -0400

    Finish two test scripts - badCommandLine.sh and indexing.sh. Improve Makefile.

--- indexer/tests/badCommandLine.sh
+++ indexer/tests/badCommandLine.sh
@@ -1,29 +1,13 @@
 echo bad arguments
-
-echo $ ../crawler 1 2 3
-../crawler 1 2 3
-
-echo $ ../crawler http://whattheheckisthis1234567890.com . 2
-../crawler http://whattheheckisthis1234567890.com . 2
-
-echo $ ../crawler http://old-www.cs.dartmouth.edu/~cs50/index.htmlllllllllllllll . 2
-../crawler http://old-www.cs.dartmouth.edu/~cs50/index.htmlllllllllllllll . 2
-
-echo $ ../crawler http://old-www.cs.dartmouth.edu/~cs50/index.html thiedirectorydoesntexist 2
-../crawler http://old-www.cs.dartmouth.edu/~cs50/index.html thiedirectorydoesntexist 2
-
-echo $ ../crawler http://old-www.cs.dartmouth.edu/~cs50/index.html . -10
-../crawler http://old-www.cs.dartmouth.edu/~cs50/index.html . -10
-
-echo insufficient arguments
-
-echo $ ../crawler 
-../crawler 
-
-echo $ ../crawler http://old-www.cs.dartmouth.edu/~cs50/index.html
-../crawler http://old-www.cs.dartmouth.edu/~cs50/index.html 
-
-echo $ ../crawler http://old-www.cs.dartmouth.edu/~cs50/index.html .
-../crawler http://old-www.cs.dartmouth.edu/~cs50/index.html .
-
-rm -f .crawler
+echo
+echo $ ../indexer 1
+../indexer 1
+echo
+echo $ ../indexer 1 2 3
+../indexer 1 2 3
+echo
+echo $ ../indexer notADirectory output/mine.small
+../indexer notADirectory output/mine.small
+echo
+echo $ ../indexer ~cs50/demo/crawler-output-small/ ./output
+../indexer ~cs50/demo/crawler-output-small/ ./output
--- indexer/tests/indexing.sh
+++ indexer/tests/indexing.sh
@@ -1,6 +1,16 @@
+# expected to be run in the same directory as this script is in
 echo comparing sorted index file outputs
 echo "the 'demo' file is sorted from ~cs50/demo/index.small"
-echo "the 'mine' file is sorted from the index file produced"
-echo "sorted with 'indexsort.awk', compared with 'diff'"
+echo "the 'mine' file is sorted from the index file by my indexer, based on"
+echo "'~cs50/demo/crawler-output-small', which is a crawler directory"
+echo "sorted the index files with 'indexsort.awk'; compared with 'diff'"
+echo "if no difference, 'NO DIFFERENCE!' is echoed to stdout"
+echo
+../indexer ~cs50/demo/crawler-output-small/ output/mine.small
 
+gawk -f ./indexsort.awk ~cs50/demo/index.small > output/demo.small.sorted
+
+gawk -f ./indexsort.awk output/mine.small > output/mine.small.sorted
+
+diff output/mine.small.sorted output/demo.small.sorted && echo "NO DIFFERENCE!"
 
